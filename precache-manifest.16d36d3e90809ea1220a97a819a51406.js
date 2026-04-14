eaf0510fb38bea911b13c43c83c20c14c1acaf36
Efrem <efremg69@gmail.com>
2026-04-14 09:32:14 +0200

    Updates

---
+++ precache-manifest.16d36d3e90809ea1220a97a819a51406.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd407b2d16d48e5172adff21e64c247",
+    "url": "/CV-v1/index.html"
+  },
+  {
+    "revision": "6906cc72a734ffcec44f",
+    "url": "/CV-v1/static/css/main.e6bc4c01.chunk.css"
+  },
+  {
+    "revision": "c9daa6a2ffad32d312d5",
+    "url": "/CV-v1/static/js/2.0574f275.chunk.js"
+  },
+  {
+    "revision": "28bc9d147a79373c4500",
+    "url": "/CV-v1/static/js/3.62d5139d.chunk.js"
+  },
+  {
+    "revision": "6906cc72a734ffcec44f",
+    "url": "/CV-v1/static/js/main.ca0ef50c.chunk.js"
+  },
+  {
+    "revision": "f044472f16c561c4fb96",
+    "url": "/CV-v1/static/js/runtime~main.c62dba33.js"
+  },
+  {
+    "revision": "c3fd69472d7f6928dbe09cfeca9d4cc2",
+    "url": "/CV-v1/static/media/bg-img.c3fd6947.jpg"
+  },
+  {
+    "revision": "b7b20e4fad78cd8d22afb13eed0b0e3d",
+    "url": "/CV-v1/static/media/grey-sky.b7b20e4f.jpg"
+  },
+  {
+    "revision": "9386b089dc34b86c144d4cd56f687425",
+    "url": "/CV-v1/static/media/mountains.9386b089.jpg"
+  },
+  {
+    "revision": "4eaaabcdb1a5b7ac597d86a9fe681cb0",
+    "url": "/CV-v1/static/media/mypic.4eaaabcd.png"
+  },
+  {
+    "revision": "b5915a1eaa9e7a78c3fb0f149cd06f8a",
+    "url": "/CV-v1/static/media/snow1.b5915a1e.jpg"
+  },
+  {
+    "revision": "6bfcf04cdc924ca6977f8f1a4a1eda67",
+    "url": "/CV-v1/static/media/sunset.6bfcf04c.jpg"
+  },
+  {
+    "revision": "f118d04ea03b8eeb57bc2edf3afd789e",
+    "url": "/CV-v1/static/media/tree.f118d04e.jpg"
+  }
+]);
